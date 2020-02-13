8ce184d3a900834cb57fc6c67ccae6dbcc0ad297
hkayaoguzhan <oguzhanharmankayaa@gmail.com>
2020-02-13 16:43:36 +0300

    /director route

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 const indexRouter = require('./routes/index');
 const movie = require('./routes/movie');
+const director = require('./routes/director');
 
 const app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/movie', movie);
+app.use('/api/director', director);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
@@ -39,6 +41,6 @@ app.use((err, req, res, next) => {
   // render the error page
   res.status(err.status || 500);
   res.json({ error : { message: err.message, code: err.code } });
-});
+}); 
 
 module.exports = app;
