1952c1b94707035d1a38b9652d4bc5e89c2b794e
hkayaoguzhan <oguzhanharmankayaa@gmail.com>
2020-02-13 17:36:15 +0300

    validasyon ornekleri

--- models/Director.js
+++ models/Director.js
@@ -2,9 +2,21 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DirectorSchema = new Schema({
-    name: String,
-    surname: String,
-    bio: String,
+    name: {
+        type: String,
+        maxlength: 50,
+        minlength: 2
+    },
+    surname: {
+        type: String,
+        maxlength: 50,
+        minlength: 2
+    },
+    bio: {
+        type: String,
+        maxlength: 500,
+        minlength: 10
+    },
     createdAt: {
         type:Date,
         default: Date.now
--- models/Movie.js
+++ models/Movie.js
@@ -4,12 +4,22 @@ const Schema = mongoose.Schema;
 const MovieSchema = new Schema({
     title: {
         type: String,
-        required: true
+        required: [true, '`{PATH}` alanı zorunludur.'],
+        maxlength: 50,
+        minlength: 1
     },
-    category: String,
+    category: {
+        type: String,
+        maxlength: 30,
+        minlength: 2
+    },
     country: String,
     year: Number,
-    imdb_score: Number,
+    imdb_score: {
+        type:Number,
+        max: 10,
+        min: 0
+    },
     director_id: Schema.Types.ObjectId,
     createdAt: {
         type: Date,
