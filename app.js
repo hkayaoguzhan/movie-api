c4ca0df4b8dc282277beee866ed86431ab1413d6
hkayaoguzhan <oguzhanharmankayaa@gmail.com>
2020-02-14 11:16:34 +0300

    /auth with jws

--- app.js
+++ app.js
@@ -11,8 +11,14 @@ const director = require('./routes/director');
 
 
 const app = express();
-
+//db connect
 const db = require('./helper/db')();
+//secret key
+const config = require('./config');
+app.set('api_secret_key', config.api_secret_key);
+
+//Middleware
+const verifyToken = require('./middleware/verify-token');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -25,8 +31,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/api', verifyToken);
 app.use('/api/movie', movie);
 app.use('/api/director', director);
+
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
--- helper/db.js
+++ helper/db.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
-const url = "mongodb+srv://root:123@movie-api-vi9wz.mongodb.net/test?retryWrites=true&w=majority"
+const url =
+  "mongodb+srv://root:123@movie-api-vi9wz.mongodb.net/test?retryWrites=true&w=majority";
 
 module.exports = () => {
-  mongoose.connect(url,
-    { useNewUrlParser: true, useUnifiedTopology: true }
-  );
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
   mongoose.connection.on("open", () => {
     console.log("MongoDb is Connected");
   });
